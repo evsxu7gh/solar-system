3613ef58d0e4fc429c5d40e36d6595c293f1318a
Sudhakar upadrasta <71725416+evsxu7gh@users.noreply.github.com>
2026-04-09 21:16:26 +0530

    Update app-controller.js

--- app-controller.js
+++ app-controller.js
@@ -1,28 +1,28 @@
 console.log('We are inside client.js');
 
 /* on page load  */
-window.onload = function() {
-    const planet_id = document.getElementById("planetID").value
-    console.log("onLoad - Request Planet ID - " + planet_id)
+window.onload = function () {
+    const planet_id = document.getElementById("planetID").value;
+    console.log("onLoad - Request Planet ID - " + planet_id);
 
     fetch("/os", {
-            method: "GET"
-        })
-        .then(function(res) {
+        method: "GET"
+    })
+        .then(function (res) {
             if (res.ok) {
                 return res.json();
             }
-            thrownewError('Request failed');
-        }).catch(function(error) {
+            throw new Error('Request failed');
+        })
+        .catch(function (error) {
             console.log(error);
         })
-        .then(function(data) {
-            document.getElementById('hostname').innerHTML = `Pod - ${data.os} `
-          //  document.getElementById('environment').innerHTML = ` Env - ${data.env}  `
+        .then(function (data) {
+            if (!data) return;
+            document.getElementById('hostname').innerHTML = `Pod - ${data.os} `;
+            // document.getElementById('environment').innerHTML = ` Env - ${data.env}  `
         });
 };
-
-
 
 const btn = document.getElementById('submit');
 if (btn) {
@@ -30,38 +30,41 @@ if (btn) {
 }
 
 function func() {
-    const planet_id = document.getElementById("planetID").value
-    console.log("onClick Submit - Request Planet ID - " + planet_id)
+    const planet_id = document.getElementById("planetID").value;
+    console.log("onClick Submit - Request Planet ID - " + planet_id);
 
     fetch("/planet", {
-            method: "POST",
-            body: JSON.stringify({
-                id: document.getElementById("planetID").value
-            }),
-            headers: {
-                "Content-type": "application/json; charset=UTF-8"
-            }
-        })
-        .then(function(res2) {
+        method: "POST",
+        body: JSON.stringify({
+            id: document.getElementById("planetID").value
+        }),
+        headers: {
+            "Content-type": "application/json; charset=UTF-8"
+        }
+    })
+        .then(function (res2) {
             if (res2.ok) {
                 return res2.json();
             }
-            thrownewError('Request failed.');
-        }).catch(function(error) {
-            alert("Ooops, We have 8 planets.\nSelect a number from 0 - 8")
+            throw new Error('Request failed.');
+        })
+        .catch(function (error) {
+            alert("Ooops, We have 8 planets.\nSelect a number from 0 - 8");
             console.log(error);
         })
-        .then(function(data) {
-            document.getElementById('planetName').innerHTML = ` ${data.name} `
+        .then(function (data) {
+            if (!data) return;
+
+            document.getElementById('planetName').innerHTML = `${data.name}`;
 
             const element = document.getElementById("planetImage");
-            const image = ` ${data.image} `
-            element.style.backgroundImage  = "url("+image+")"
+            element.style.backgroundImage = `url('/images/${data.image}')`;
+            element.style.backgroundRepeat = "no-repeat";
+            element.style.backgroundPosition = "center center";
+            element.style.backgroundSize = "cover";
 
-            const planet_description = ` ${data.description} `
-            document.getElementById('planetDescription').innerHTML = planet_description.replace(/(.{80})/g, "$1<br>");
-
-          
+            const planet_description = `${data.description}`;
+            document.getElementById('planetDescription').innerHTML =
+                planet_description.replace(/(.{80})/g, "$1<br>");
         });
-
 }
